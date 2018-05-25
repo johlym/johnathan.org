8fcd4e25258c7bd91544e097c0a4afc3a3706a72
Johnathan Lyman <jelyman@gmail.com>
2018-05-24 18:22:32 -0700

    add npm and gulp stuff

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var gulp = require('gulp');
+var htmlmin = require('gulp-htmlmin');
+var runSequence = require('run-sequence');
+var uglify = require('gulp-uglify');
+
+// Gulp task to minify HTML files
+gulp.task('pages', function() {
+  return gulp.src(['./_site/**/*.html'], {base: './'})
+    .pipe(htmlmin({
+      collapseWhitespace: true,
+      removeComments: true
+    }))
+    .pipe(gulp.dest('./'));
+});
+
+// Gulp task to minify JavaScript files
+gulp.task('scripts', function() {
+  return gulp.src('./_site/**/*.js', {base: './'})
+    // Minify the file
+    .pipe(uglify())
+    // Output
+    .pipe(gulp.dest('./'))
+});
+
+// Gulp task to minify all files
+gulp.task('default', ['min'], function () {
+  runSequence(
+    'scripts',
+    'pages'
+  );
+});
